d037c82c810fb709a17305625495a5b69a13fe91
kehuo <hkhuoke@hotmail.com>
2020-04-04 13:34:25 +0800

    update deploy related files such start.sh, dockerfile, etc.

--- backend/start.sh
+++ backend/start.sh
@@ -2,5 +2,4 @@ export LANG=en_US.UTF8
 export CFG_PATH=/project/service.conf
 
 /usr/sbin/nginx
-cd project
-/root/miniconda3/envs/myenv/bin/python server.py
+/root/miniconda3/envs/myenv/bin/python /project/server.py
