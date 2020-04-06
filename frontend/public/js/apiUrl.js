277434452baee85b6a5cda2bff80595d1a06cf04
kehuo <hkhuoke@hotmail.com>
2020-04-06 14:08:14 +0800

    docker and .less file fix

--- frontend/public/js/apiUrl.js
+++ frontend/public/js/apiUrl.js
@@ -1,6 +1,6 @@
 window.OpApi = "http://localhost:5000/api/v1";
 //window.OpApi = 'http://172.18.0.41:6313/api/v1';
-
+//123
 window.modulesSupport = ["welcome", "exam-standard"];
 // 'template', 'third-party', 'tagging-normal', 'train-model',
 // 'system-operation', 'statistics', 'dashboarder-builder',
