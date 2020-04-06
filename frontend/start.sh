bf3662d62088dda9c0d0bf24e5454f054e6863cf
kehuo <hkhuoke@hotmail.com>
2020-04-06 15:35:53 +0800

    fix docker build

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,1 +1,8 @@
-/usr/local/bin/npm run start
+# macbook 的npm 所在路径是 /usr/local/bin/npm 
+#/usr/local/bin/npm run start
+
+# 我的 kehuo/centos8_conda_npm_nginx:v1 的 npm 命令所在路径是 /usr/bin/npm
+# /usr/bin/npm run start
+
+# 运行以下命令, 启动 docker, 这个docker运行这 打包好的前端项目, 和 nginx.
+/usr/sbin/nginx
