d54772f728b737b634b6f4dd3e622d08fd335f51
kehuo <hkhuoke@hotmail.com>
2020-04-07 21:54:32 +0800

    apiUrl update https for backend api url

--- frontend/public/js/apiUrl.js
+++ frontend/public/js/apiUrl.js
@@ -1,5 +1,5 @@
 //window.OpApi = "http://127.0.0.1:5000/api/v1";
-window.OpApi = "http://kevinhuo.cool:5000/api/v1";
+window.OpApi = "https://kevinhuo.cool:5000/api/v1";
 
 // if you want to create new route, first of all, need to add parent route name to the below array.
 window.modulesSupport = [
